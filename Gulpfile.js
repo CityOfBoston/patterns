11db7019e44bf24df76f2a3da39feee0d1739362
Matthew Crist <matthew.crist@boston.gov>
2016-12-06 16:43:05 -0500

    Added the image step to the build

--- Gulpfile.js
+++ Gulpfile.js
@@ -37,5 +37,5 @@ gulp.task('watch:scripts', getTask('scripts_watch'));
 gulp.task('stylus', getTask('stylus'));
 gulp.task('stylus:ie', getTask('stylus_IE'));
 gulp.task('watch:stylus', getTask('stylus_watch'));
-gulp.task('build', ['stylus', 'scripts', 'stylus:ie']);
+gulp.task('build', ['images', 'stylus', 'scripts', 'stylus:ie']);
 gulp.task('default', ['images', 'scripts', 'watch:scripts', 'stylus', 'stylus:ie', 'watch:stylus']);
