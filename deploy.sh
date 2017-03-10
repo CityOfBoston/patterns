c531ec608e25784ef2bc9199ba4b292565aa128a
Travis CI <matthewcrist@hondo.cityhall.boston.cob>
2017-03-10 10:40:30 -0500

    Setup key in build process

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,16 @@ then
   exit 0
 fi
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in patterns.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 REV=$(git rev-parse --short HEAD)
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
