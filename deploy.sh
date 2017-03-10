dea92ca8aaabd0a8ce990a00fc822299162ae77b
Travis CI <matthewcrist@hondo.cityhall.boston.cob>
2017-03-10 11:54:31 -0500

    Stop using keys

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -o errexit -o nounset
 
 if [ "$TRAVIS_BRANCH" != "master" ]
@@ -7,27 +8,15 @@ then
   exit 0
 fi
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in crispus -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-REV=$(git rev-parse --short HEAD)
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+rev=$(git rev-parse --short HEAD)
 
 cd public
 
 git init
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-git remote add upstream $SSH_REPO
+git config user.name "City of Boston Digital Team"
+git config user.email "digital@boston.gov"
 
+git remote add upstream "https://$GH_TOKEN@github.com/cityofboston/patterns.git"
 git fetch upstream
 git reset upstream/gh-pages
 
