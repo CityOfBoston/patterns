bdc588f9c839c70d3d3d5cfb4d286fc488a72449
Matthew Crist <matthew.crist@boston.gov>
2017-03-10 10:12:19 -0500

    Update build

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ SHA=`git rev-parse --verify HEAD`
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO public
-cd out
+cd public
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
