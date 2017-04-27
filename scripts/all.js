98139edcf6728a8712dda78cbb68d360c9f96c2d
City of Boston Digital Team <digital@boston.gov>
2017-04-27 23:24:44 +0000

    rebuild pages at 68ac419

--- scripts/all.js
+++ scripts/all.js
@@ -1,4 +1,25 @@
 'use strict'
+// This module provides helpers for Boston.gov
+// ---------------------------
+var Boston = (function () {
+  // Set height
+  var trigger;
+  var container;
+  var video_id;
+  var video_channel;
+
+  // Returns the child element based on selector
+  // Parent needs an ID
+  function child(el, selector) {
+    return document.querySelectorAll('#' + el.id + ' ' + selector);
+  }
+
+  return {
+    child: child
+  }
+})();
+
+'use strict'
 // This module controls the City of Boston seal
 // ---------------------------
 // On scroll, the seal will hide or show depending on the
@@ -138,3 +159,58 @@ var BostonTabs = (function () {
 })()
 
 BostonTabs.start()
+
+'use strict'
+// This module controls the City of Boston video component
+// ---------------------------
+var BostonVideo = (function () {
+  // Set height
+  var video = {};
+
+  function getURL(video_id, video_channel) {
+    if (video_channel == 'true') {
+      return "https://www.youtube.com/embed/live_stream?autoplay=1&channel=" + video_id;
+    } else {
+      return "https://www.youtube.com/embed/" + video_id + "?autoplay=1";
+    }
+  }
+
+  function handleTrigger(video) {
+    var embed_url = getURL(video.el.getAttribute('data-vid-id'), video.el.getAttribute('data-vid-channel'));
+
+    video.container.innerHTML = '<iframe src="' + embed_url + '" class="vid-v"></iframe>'
+  }
+
+  function initVideo(video) {
+    video[video.id] = {
+      el: video,
+      container: Boston.child(video, '.vid-c')[0],
+      trigger: Boston.child(video, '.vid-cta')[0]
+    };
+
+    // Set click events on each
+    video[video.id].el.addEventListener('click', function(ev) {
+      ev.preventDefault();
+      ev.stopPropagation();
+
+      handleTrigger(video[video.id]);
+    });
+  }
+
+  // Pass the ID of the video
+  function start(video_id) {
+    var videos = document.querySelectorAll('.vid');
+
+    if (videos.length > 0) {
+      for (var i = 0; i < videos.length; i++) {
+        initVideo(videos[i]);
+      }
+    }
+  }
+
+  return {
+    start: start
+  }
+})();
+
+BostonVideo.start();
