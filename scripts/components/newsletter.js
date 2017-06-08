27c1b989d8764dca00a94c224b754f79fa5b3f9c
Matthew Crist <matthew.crist@boston.gov>
2017-06-08 11:23:03 -0400

    Update newsletter validation

--- scripts/components/newsletter.js
+++ scripts/components/newsletter.js
@@ -14,6 +14,8 @@ var BostonNewsletter = (function () {
       var formData = new FormData(form);
 
       if (isValid) {
+        disableButton(form);
+
         Boston.request({
           data: formData,
           url: form.getAttribute('action'),
@@ -27,10 +29,35 @@ var BostonNewsletter = (function () {
               handleError();
             }
           },
-          error: handleError
+          error: function() {
+            enableButton(form)
+            handleError();
+          }
         });
       }
     });
+  }
+
+  function disableButton(form) {
+    var button = Boston.childByEl(form, 'btn');
+
+    if (button.length > 0) {
+      for (var i = 0; i < button.length; i++) {
+        button[i].disabled = true;
+        button[i].innerHTML = 'Loading...';
+      }
+    }
+  }
+
+  function enableButton(form) {
+    var button = Boston.childByEl(form, 'btn');
+
+    if (button.length > 0) {
+      for (var i = 0; i < button.length; i++) {
+        button[i].disabled = false;
+        button[i].innerHTML = 'Sign Up';
+      }
+    }
   }
 
   function handleError() {
@@ -39,13 +66,24 @@ var BostonNewsletter = (function () {
 
   function validateForm(form) {
     var email = Boston.childByEl(form, 'bos-newsletter-email');
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var zip = Boston.childByEl(form, 'bos-newsletter-zip');
 
-    if (email[0].value == '' || !re.test(email[0].value)) {
+    var emailRE = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+    var zipRE = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
+
+    if (email[0].value == '' || !emailRE.test(email[0].value)) {
       var errors = document.createElement('div');
       errors.className = "t--subinfo t--err m-t100";
       errors.innerHTML = "Please enter a valid email address";
       email[0].parentElement.appendChild(errors);
+      return false;
+    }
+
+    if (zip[0].value !== '' && !zipRE.test(zip[0].value)) {
+      var errors = document.createElement('div');
+      errors.className = "t--subinfo t--err m-t100";
+      errors.innerHTML = "Please enter a valid zip code";
+      zip[0].parentElement.appendChild(errors);
       return false;
     }
 
