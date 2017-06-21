6bc5058c1c508cb3fc26449da87d1ccb9ac6bafd
Matthew Crist <matthew.crist@boston.gov>
2017-06-21 09:05:21 -0400

    Start on printing

--- gulp-tasks/stylus.js
+++ gulp-tasks/stylus.js
@@ -4,7 +4,8 @@ module.exports = function (gulp, plugins, options) {
   return function () {
     var stream = gulp.src([
         './stylesheets/public.styl',
-        './stylesheets/hub.styl'
+        './stylesheets/hub.styl',
+        './stylesheets/print.styl'
       ])
       .pipe(plugins.stylus({
         use: [
