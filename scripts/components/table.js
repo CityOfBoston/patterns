6b85590c9f4beaf02b54f858642d627765d3b0db
jimafisk <jimafisk@gmail.com>
2017-07-27 15:40:02 -0400

    Start rewriting vertical table transpose logic.

--- scripts/components/table.js
+++ scripts/components/table.js
@@ -3,9 +3,79 @@
 // ---------------------------
 var BostonTable = (function () {
 
-  function doit() {
-    console.log('table');
-  }
+  // Transform a vertical header table into a horizontal header table so that
+  // it can utilize horizontal header responsive styling.
+  //if (window.enquire !== undefined) {
+    function transposeHorizontal() {
+      var $this = $(this),
+        table = $('<table><thead><tr></tr></thead><tbody></tbody></table>'),
+        newRows = [];
+      // Add vertical row th elements to the thead element of the horizontal
+      // table.
+      $('th', this).each(function () {
+        $('thead > tr', table).append('<th>' + this.innerHTML + '</th>');
+      });
+      $this.find('tbody > tr').each(function() {
+        var i = 0;
+        $(this).find('td').each(function () {
+          if (newRows[i] === undefined) {
+            newRows[i] = $("<tr></tr>");
+          }
+          newRows[i++].append($(this));
+        });
+      });
+      $.each(newRows, function() {
+        $('tbody', table).append(this);
+      });
+
+      // After we've built up the table, add classes and then replace the
+      // current table's HTML with the one we've built up.
+      $this.addClass('responsive-table responsive-table--vertical transposed');
+      $this.html(table.html());
+    };
+    // Transform a horizontal header table back into a vertical header table so
+    // that it can be viewed properly at desktop screen sizes.
+    function transposeVertical() {
+      var $this = $(this),
+        table = $('<table><tbody></tbody></table>'),
+        newRows = [];
+      $this.find('tbody > tr').each(function() {
+        var i = 0;
+        $(this).find('td').each(function() {
+          if (newRows[i] === undefined) {
+            newRows[i] = $("<tr></tr>");
+          }
+          newRows[i++].append($(this));
+        });
+      });
+      // Add th elements at the beginning of each row.
+      $this.find('th').each(function(index, value) {
+        newRows[index].prepend(this);
+      });
+      $.each(newRows, function() {
+        $('tbody', table).append(this);
+      });
+
+      // After we've built up the table, add classes and then replace the
+      // current table's HTML with the one we've built up.
+      $this.removeClass('transposed');
+      $this.addClass('responsive-table responsive-table--vertical');
+      $this.html(table.html());
+    };
+
+    /*
+    enquire.register("screen and (min-width: 768px)", {
+      match: function() {
+        $('.responsive-table--vertical.transposed').transposeVertical();
+      }
+    })
+    .register("screen and (max-width: 767px)", {
+      match: function() {
+        $('.responsive-table--vertical').transposeHorizontal();
+      }
+    });
+    */
+  //}
 
   function start() {
     // Check for vertical tables
@@ -13,7 +83,8 @@ var BostonTable = (function () {
 
     // If there are vertical tables, run...
     if (tables.length > 0) {
-      doit();
+      transposeHorizontal();
+      transposeVertical();
     }
   }
   return {
