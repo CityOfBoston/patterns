183b356e83fcaa905c9491a74a821fb8ffceac8d
jimafisk <jimafisk@gmail.com>
2017-08-01 15:54:57 -0400

    Clean up code.

--- scripts/components/table.js
+++ scripts/components/table.js
@@ -3,26 +3,29 @@
 // ---------------------------
 var BostonTable = (function () {
 
-  function columnToTable(tables, reset) {
+  function transposeTable(tables, reset) {
     // Loop through all vertical tables on the page.
     for (var i = 0, length = tables.length; i < length; i++) {
+      // If going back to a large screen, use original vertical table style.
       if (reset) {
-        //console.log(tables[i]);
+        // Get all the tables as currently displayed.
         var updatedTables = document.querySelectorAll('.responsive-table--vertical');
+        // Loop through all tables.
         for (var j = 0, length = updatedTables.length; j < length; j++) {
+          // Make sure you replace the current table with the correct original table.
           if (i == j) {
-            console.log("Existing: ");
-            console.log(updatedTables[j]);
-            console.log("");
-            console.log("Orig: ");
-            console.log(tables[i]);
+            // Actually replace the table HTML.
             updatedTables[j].replaceWith(tables[i]);
           }
         }
+        // Exit so the transpose code does not run.
         return;
       }
+      // Create an empty table element.
       var newTable = document.createElement('table');
+      // Set the classes on the empty table.
       newTable.setAttribute('class', 'responsive-table responsive-table--vertical');
+      // Initialize the maximum number of columns to loop through.
       var maxColumns = 0;
       // Find the max number of columns
       for(var r = 0; r < tables[i].rows.length; r++) {
@@ -62,12 +65,10 @@ var BostonTable = (function () {
 
     // If there are vertical tables, and we're on a mobile screen, run...
     if (tables.length > 0 && width <= 768) {
-      //console.log("small");
-      columnToTable(tables);
+      transposeTable(tables);
     }
     else {
-      //console.log("large");
-      columnToTable(tables, "reset");
+      transposeTable(tables, "reset");
     }
     };
   }
