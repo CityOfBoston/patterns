e598bfb78ecad19995f23631ffed31a0205db06c
jimafisk <jimafisk@gmail.com>
2017-08-01 14:51:52 -0400

    Add data-label to new table.

--- scripts/components/table.js
+++ scripts/components/table.js
@@ -18,14 +18,16 @@ var BostonTable = (function () {
       for(var c = 0; c < maxColumns; c++) {
         newTable.insertRow(c);
         for(var r = 0; r < tables[i].rows.length; r++) {
-          console.log(tables[i].rows[r].cells[c].tagName);
           if(tables[i].rows[r].length <= c) {
             newTable.rows[c].insertCell(r);
             newTable.rows[c].cells[r] = '-';
           }
           else if (tables[i].rows[r].cells[c].tagName != 'TH') {
             newTable.rows[c].insertCell(r);
+            // Set the table data value.
             newTable.rows[c].cells[r].innerHTML = tables[i].rows[r].cells[c].innerHTML;
+            // Set the table data attributes used by pseudo class.
+            newTable.rows[c].cells[r].setAttribute('data-label', tables[i].rows[r].cells[c].getAttribute('data-label'));
           }
         }
       }
