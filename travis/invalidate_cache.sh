0171b03f376417408074b202e74ef8bf0aaa4e56
Fin Hopkins <fin.hopkins@boston.gov>
2018-02-21 10:41:37 -0500

    Disables Percy on production branch
    Fix for #245
    
    Also removes unnecessary (and never triggered) branch check for
    invalidating the cache. Travis only runs the script after a deploy in
    any case.

--- travis/invalidate_cache.sh
+++ travis/invalidate_cache.sh
@@ -2,12 +2,6 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
 # Invalidate CloudFront distribution
 pip install --upgrade --user awscli
 aws configure set preview.cloudfront true
--- travis/percy.sh
+++ travis/percy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-percy snapshot public --snapshots_regex="(components\/preview.).*\.html$" --enable_javascript --trace --widths "375,1024,1280"
+if [ "$TRAVIS_BRANCH" != "production" ]
+then
+  percy snapshot public --snapshots_regex="(components\/preview.).*\.html$" --enable_javascript --trace --widths "375,1024,1280"
+fi
