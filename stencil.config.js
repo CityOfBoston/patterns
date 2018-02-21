d3303ab0a8f3a6e15be9095920c9e1328423d616
Fin Hopkins <fin.hopkins@boston.gov>
2018-02-21 13:30:49 -0500

    Stencil fixes
     - Upgrades to 0.6.5
     - Changes config to remove publicPath, which brings back support for
       hosting components on one domain and loading them on another.
     - Workarounds in the config for various Stencil behaviors.

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,4 @@
 exports.config = {
-  srcDir: 'web-components',
-
   namespace: 'all',
   buildEs5: true,
 
@@ -8,12 +6,25 @@ exports.config = {
   // out.
   serviceWorker: false,
 
-  // URL where we want our components, index.html, and sw.js to live
-  publicPath: '/web-components/all/',
-  // Directory that maps to the public path.
-  wwwDir: 'assets/web-components/',
-  // We want the components to be built directly into /web-components/
-  buildDir: '.',
+  srcDir: 'web-components',
+
+  // Directory that maps to the server’s /
+  wwwDir: 'assets',
+  generateWWW: true,
+  // Since the wwwDir ('assets') is shared by Fractal, we can't delete it.
+  emptyWWW: false,
+
+  // Relative to wwwDir. Keeps our components compartmentalized in the Fleet
+  // library.
+  buildDir: 'web-components',
+
+  // Because of https://github.com/ionic-team/stencil/issues/563 we don't use
+  // the built-in index.html support, since if it gets written straight into the
+  // assets/ directory then it replaces the Fractal home page. As a workaround,
+  // we copy a different index.html file into web-components.
+  //
+  // It won't be pre-rendered, but that's fine.
+  copy: [{ src: 'html', dest: 'web-components' }],
 
   plugins: [
     // We use PostCSS with autoprefixer to let us use flexbox in component CSS
