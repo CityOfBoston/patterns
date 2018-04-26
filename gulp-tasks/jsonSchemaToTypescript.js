5c0bedd00f656aee2f7d2c8cdd71b32a8b878b14
Fin Hopkins <fin.hopkins@boston.gov>
2018-04-26 13:43:31 -0400

    Adds support for VizWiz schema v1.0
    Includes a Gulp task to pull the schema files from the vizwiz docs and
    turn them into TypeScript type definitions.
    
    Adds converting from new field values within the <cob-viz> element.
    
    Also moves Prettier config to .prettierrc.json so that other tools can
    see it.

---
+++ gulp-tasks/jsonSchemaToTypescript.js
@@ -0,0 +1,39 @@
+/**
+ * @fileOverview Gulp task that takes JSON-Schema documents and converts them to
+ * TypeScript type definitions.
+ */
+
+const path = require('path');
+const gutil = require('gulp-util');
+const PluginError = require('plugin-error');
+const through = require('through2');
+const {
+  compile: jsonSchemaToTypescript,
+} = require('json-schema-to-typescript');
+
+const PLUGIN_NAME = 'jsonSchemaToTypescript';
+
+module.exports = ({ style }) =>
+  through.obj(function(file, enc, cb) {
+    if (file.isStream()) {
+      this.emit(
+        'error',
+        new PluginError(PLUGIN_NAME, 'Streams are not supported!')
+      );
+      return cb();
+    }
+
+    const schema = JSON.parse(file.contents.toString(enc));
+
+    jsonSchemaToTypescript(schema, undefined, {
+      style,
+    }).then(typescript => {
+      this.push(
+        new gutil.File({
+          path: path.basename(file.path).replace('.json', '.d.ts'),
+          contents: new Buffer(typescript, 'utf-8'),
+        })
+      );
+      cb();
+    });
+  });
