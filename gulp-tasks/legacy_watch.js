ed79358b99388add529f14a091de5eea8aefc72a
David <david.upton@boston.gov>
2018-06-27 14:29:24 -0400

    Adds legacy .styl files into gulp watch to build dynamically.

---
+++ gulp-tasks/legacy_watch.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function(gulp) {
+  return function() {
+    var stream = gulp.watch(
+      ['./legacy/**.styl'], ['legacy']
+    );
+
+    return stream;
+  };
+};
