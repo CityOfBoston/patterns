684c3e3be514e34814c8c9818283b85f2d005f10
David Upton <davidrkupton@users.noreply.github.com>
2025-01-24 10:58:08 -0500

    Revert "(develop) Clean up Javascript; Refactor headers on Boston.gov"

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,5 +1,5 @@
 'use strict'
-// This module controls the City of Boston header component
+// This module controls the City of Boston newsletter component
 // ---------------------------
 var BostonHeader = (function () {
   var guideTitle;
@@ -78,15 +78,6 @@ var BostonHeader = (function () {
 
     if (burgerckbx) {
       burgerckbx.setAttribute("tabIndex", "0");
-      burgerckbx.addEventListener("click", function (element) {
-        if (document.getElementById("cob-content").classList.contains('brg-tr--active')) {
-          document.getElementById("cob-content").classList.remove('brg-tr--active');
-        }
-        else {
-          document.getElementById("cob-content").classList.add('brg-tr--active');
-        }
-      });
-
     }
 
     if (searchckbx) {
